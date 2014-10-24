5142f48f1c73a5551833beef3f5ac5f941c84b95
BenS <bsande16@uncc.edu>
2014-10-24 15:09:01 -0300

    please work

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Myrottenpotatoes
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.assets.initialize_on_precompile = false
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
